9ffa9ef1416fe3bed0ea67dc2f2482e9e6b96d31
djware <85318457+djware@users.noreply.github.com>
2024-03-13 08:50:44 -0500

    Update launch.sh

--- Tools/BootLogo/launch.sh
+++ Tools/BootLogo/launch.sh
@@ -2,46 +2,46 @@
 
 LOG_FILE="/mnt/SDCARD/logologs.log"
 
-
 exec > $LOG_FILE 2>&1
 
 SOURCE_FILE="/mnt/SDCARD/logo.bmp"
+ALTERNATIVE_SOURCE_FILE="/mnt/UDISK/logo.bmp"
 TARGET_PARTITION="/dev/mmcblk0p1"
 MOUNT_POINT="/mnt/emmcblk0p1"
-
 
 echo "Mounting $TARGET_PARTITION to $MOUNT_POINT..."
 mkdir -p $MOUNT_POINT
 mount $TARGET_PARTITION $MOUNT_POINT
-
 
 if [ $? -ne 0 ]; then
     echo "Failed to mount $TARGET_PARTITION."
     exit 1
 fi
 
-
 if [ -f $SOURCE_FILE ]; then
-    
     echo "Moving $SOURCE_FILE to $MOUNT_POINT/bootlogo.bmp..."
     cp $SOURCE_FILE $MOUNT_POINT/bootlogo.bmp
-
-    
-    if [ $? -ne 0 ]; then
-        echo "Failed to move file."
-    else
-        echo "File moved successfully."
-    fi
+elif [ -f $ALTERNATIVE_SOURCE_FILE ]; then
+    echo "Moving $ALTERNATIVE_SOURCE_FILE to $MOUNT_POINT/bootlogo.bmp..."
+    cp $ALTERNATIVE_SOURCE_FILE $MOUNT_POINT/bootlogo.bmp
 else
-    echo "Source file $SOURCE_FILE does not exist."
+    echo "Source file does not exist in /mnt/SDCARD or /mnt/UDISK."
+    echo "Unmounting $TARGET_PARTITION..."
+    umount $TARGET_PARTITION
+    rmdir $MOUNT_POINT
+    exit 1
 fi
 
+
+if [ $? -ne 0 ]; then
+    echo "Failed to move file."
+else
+    echo "File moved successfully."
+fi
 
 echo "Unmounting $TARGET_PARTITION..."
 umount $TARGET_PARTITION
 
-
 rmdir $MOUNT_POINT
 
 echo "Script completed."
-
